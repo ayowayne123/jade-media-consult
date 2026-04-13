94166acf2a4c0bf8e7dfd7a08016768ef15ccd2a
ayowayne123 <ayowayne123@gmail.com>
2026-04-14 00:30:50 +0100

    Feat: Contact form mobile done

--- app/contact/contact-form.js
+++ app/contact/contact-form.js
@@ -3,20 +3,20 @@ import Button from "@/components/ui/button";
 
 function ContactForm() {
   return (
-    <form className="bg-black py-16 px-8 lg:rounded-4xl">
-      <div className="space-y-6">
+    <form className="bg-black py-8 sm:py-12 lg:py-16 px-4 sm:px-6 lg:px-8 lg:rounded-4xl rounded-3xl">
+      <div className="space-y-6 font-semibold lg:font-normal">
         <input
           type="text"
-          className="w-full bg-white/10 border border-black/20 py-3.5 px-10 rounded-2xl lg:rounded-4xl text-white/90 placeholder:text-white/50 text-xl focus:outline-2 appearance-none outline-brand/80"
+          className="w-full bg-white/10 border border-black/20 py-2 lg:py-3.5 px-4 sm:px-6 lg:px-10 rounded-2xl lg:rounded-4xl text-white/90 placeholder:text-white/50 text-xl focus:outline-2 appearance-none outline-brand/80"
           placeholder="Brand name or company name"
         />
         <input
           type="email"
-          className="w-full bg-white/10 border border-black/20 py-3.5 px-10 rounded-2xl lg:rounded-4xl text-white/90 placeholder:text-white/50 text-xl focus:outline-2 appearance-none outline-brand/80"
+          className="w-full bg-white/10 border border-black/20 py-2 lg:py-3.5 px-4 sm:px-6 lg:px-10 rounded-2xl lg:rounded-4xl text-white/90 placeholder:text-white/50 text-xl focus:outline-2 appearance-none outline-brand/80"
           placeholder="Email"
         />
         <textarea
-          className="w-full bg-white/10 border border-black/20 py-3.5 px-10 rounded-2xl lg:rounded-4xl text-white/90 placeholder:text-white/50 text-xl focus:outline-2 resize-none outline-brand/80"
+          className="w-full bg-white/10 border border-black/20 py-2 lg:py-3.5 px-4 sm:px-6 lg:px-10 rounded-2xl lg:rounded-4xl text-white/90 placeholder:text-white/50 text-xl focus:outline-2 resize-none outline-brand/80"
           placeholder="Tell us how we can help you"
           rows={6}
         />
